e5880ebc04055052192e2a9e165b7df922110483
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-05-15 12:52:55 -0400

    Install corrrect dependencies for NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NavBar from "./components/navigation/NavBar";
 import Home from "./components/static/Home";
 
 const App = () => {
   return (
     <div className="App">
+      <NavBar />
      <Home />
     </div>
   );
--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,41 +1,34 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-const NavBar = ()=> {
-  const classes = useStyles();
-
+const NavBar = () => {
   return (
-    <div className={classes.root}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Add Activity</Button>
         </Toolbar>
       </AppBar>
-    </div>
+    </Box>
   );
 }
 
-export default NavBar
+export default NavBar;

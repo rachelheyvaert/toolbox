123ae843fca79dd8cead2ac47ace4327463d789b
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-05-17 19:55:03 -0400

    Adds styling for new activity form

--- src/components/NewActivityForm.jsx
+++ src/components/NewActivityForm.jsx
@@ -1,16 +1,69 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import FilledInput from '@mui/material/FilledInput';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
-const NewActivityForm = () => {
+const NewActivityForm =() =>{
+  const [name, setName] = React.useState('');
 
-  //patch request
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>Add an Activity to your ToolBox
-        <form>
-
-        </form>
-      </h1></div>
-  )
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 14 },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <FormControl variant="standard">
+        <InputLabel htmlFor="component-helper">Name of Activity</InputLabel>
+        <Input
+          id="component-helper"
+          value={name}
+          onChange={handleChange}
+          aria-describedby="component-helper-text"
+        />
+        <FormHelperText id="component-helper-text">
+          Name of Activity..
+        </FormHelperText>
+      </FormControl>
+      <FormControl disabled variant="standard">
+        <InputLabel htmlFor="component-disabled">Name</InputLabel>
+        <Input id="component-disabled" value={name} onChange={handleChange} />
+        <FormHelperText>Disabled</FormHelperText>
+      </FormControl>
+      <FormControl error variant="standard">
+        <InputLabel htmlFor="component-error">Name</InputLabel>
+        <Input
+          id="component-error"
+          value={name}
+          onChange={handleChange}
+          aria-describedby="component-error-text"
+        />
+        <FormHelperText id="component-error-text">Error</FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="component-outlined">test</InputLabel>
+        <OutlinedInput
+          id="component-outlined"
+          value={name}
+          onChange={handleChange}
+          label="Name"
+        />
+      </FormControl>
+      <FormControl variant="filled">
+        <InputLabel htmlFor="component-filled">Name</InputLabel>
+        <FilledInput id="component-filled" value={name} onChange={handleChange} />
+      </FormControl>
+    </Box>
+  );
 }
-
-export default NewActivityForm
+export default NewActivityForm;
